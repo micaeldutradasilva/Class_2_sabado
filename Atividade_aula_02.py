73fa50864b4ca1621e6ff3cda30007b24ed4a9f0
Micael Dutra da Silva <119418848+micaeldutradasilva@users.noreply.github.com>
2023-09-23 17:19:55 -0300

    Add files via upload
    Aula 2 | SENAI

---
+++ Atividade_aula_02.py
@@ -0,0 +1,43 @@
+# -------------- Atividade 01 --------------
+
+# numero_01 = float(input("Digite o primeiro número: "))
+# numero_02 = float(input("Digite o segundo número: "))
+# soma = numero_01 + numero_02
+# print(soma)
+
+
+# -------------- Atividade 02 --------------
+
+# name = input("What 's your name? ")
+# print("My name is", name)
+
+# -------------- Atividade 03 --------------
+
+# name = input("What 's your name: ")
+# print("Hi,",name,", How are you?")
+
+# -------------- Atividade 04 --------------
+
+# numero_01 = float(input("Digite o primeiro número: "))
+# numero_02 = float(input("Digite o segundo número: "))
+# multiplicacao = numero_01 * numero_02
+# print(multiplicacao)
+
+# -------------- Sistema completo --------------
+
+name = input("What 's your name? ")
+print("Hi,",name,"How are you?")
+print("Let 's Go!")
+numero1 = float(input("Vamos iniciar com o primeiro número, qual primeiro número deseja somar: "))
+numero2 = float(input("Qual segundo número deseja somar: "))
+soma = numero1+numero2
+print("Very nice", name)
+print("Vamos agora a multiplicação")
+numero_01 = float(input("Qual primeiro número deseja multiplicar: "))
+numero_02 = float(input("Qual segundo número deseja multiplicar: "))
+multiplicacao = numero_01 * numero_02
+print(multiplicacao)
+print("Vamos ao resumo,",name)
+print("Você fez a seguinte soma:",numero1,"+",numero2, "No qual deu o total de", soma)
+print("Você fez a seguinte multiplicação:",numero_01,"*",numero_02,"No qual deu o total de",multiplicacao)
+print("See you later")
